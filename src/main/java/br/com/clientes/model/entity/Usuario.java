b792ea4557aee91ce66dd28bfc17c3c2fbf82cd6
thiago <tc.thiagocosta@gmail.com>
2020-08-25 15:27:25 -0300

    Incluindo assinatura unique em username

--- src/main/java/br/com/clientes/model/entity/Usuario.java
+++ src/main/java/br/com/clientes/model/entity/Usuario.java
@@ -15,6 +15,7 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Column(unique = true)
     @NotEmpty(message = "{campo.login.obrigatorio}")
     private String username;
 
